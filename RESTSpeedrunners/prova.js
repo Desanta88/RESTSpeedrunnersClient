11d1a9b08ae2e443bfbf14c751b4fea50b9bbc24
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2024-04-19 00:41:14 +0200

    implementazione di GET,POST,PUT e DELETE

---
+++ RESTSpeedrunners/prova.js
@@ -0,0 +1,8 @@
+function getRecord(){
+    alert("ciao");
+}
+
+/*var baseURL="localhost/RESTSpeedrunners/";
+    fetch(baseURL+"1")
+    .then(response=>response.json())
+    .then(json=>console.log(json));*/
