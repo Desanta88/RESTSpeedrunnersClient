ced173f6ff833cf53d7e15f274ab837e96b91b36
Desanta88 <101564257+Desanta88@users.noreply.github.com>
2024-04-29 11:48:37 +0200

    Delete RESTSpeedrunners directory

--- RESTSpeedrunners/prova.js
+++
@@ -1,8 +0,0 @@
-function getRecord(){
-    alert("ciao");
-}
-
-/*var baseURL="localhost/RESTSpeedrunners/";
-    fetch(baseURL+"1")
-    .then(response=>response.json())
-    .then(json=>console.log(json));*/
